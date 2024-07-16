c0939266afe3b03e760bb13711bec50425b09ddd
ARQ31 <49955437+ARQ31@users.noreply.github.com>
2024-07-16 09:35:39 +0700

    Update castingdata.py

--- castingdata.py
+++ castingdata.py
@@ -7,18 +7,14 @@ Author : Ariq Marwan Permana - 2024
 import os #importing os library for os.system function
 from datetime import datetime # importing datetime to get the current year
 
-# First step 
-def first(): #define first function
-    os.system('cls' if os.name == 'nt' else 'clear')  # to clear the screen based on OS
-    x = input("Siapa nama lengkap anda? : ")
-    y = input("Berapakah tahun lahir anda? : ")
-    print(type(y)) #To show you every input function on default set yielded string data type
-    print(type(x)) #Even though you input int value, it's set to string output
-    y = int(y) #This function to convert the x variable data type to integer data type
-    sekarang = datetime.now()
-    tahun = sekarang.year
-    tahun = int(tahun)
-    tahun_lahir = tahun - y #To calculate user birth year
-    print(f"umur anda sekarang adalah : {tahun_lahir} ")
-
-first()
+os.system('cls' if os.name == 'nt' else 'clear')  # to clear the screen based on OS
+x = input("Siapa nama lengkap anda? : ")
+y = input("Berapakah tahun lahir anda? : ")
+print(type(y)) #To show you every input function on default set yielded string data type
+print(type(x)) #Even though you input int value, it's set to string output
+y = int(y) #This function to convert the x variable data type to integer data type
+sekarang = datetime.now()
+tahun = sekarang.year
+tahun = int(tahun)
+tahun_lahir = tahun - y #To calculate user birth year
+print(f"umur anda sekarang adalah : {tahun_lahir} ")
